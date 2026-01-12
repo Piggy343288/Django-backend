74a048538db2c27a3272fce90f944d989fe856cd
Piggy343288 <155237651+Piggy343288@users.noreply.github.com>
2026-01-12 11:32:23 +0800

    Add files via upload

--- mainsite/piggySQL/admin.py
+++ mainsite/piggySQL/admin.py
@@ -1,25 +1,29 @@
 from django.contrib import admin
-from .models import Tag, User, File, Plan, Article
-from .models import Comment, Paper, ChatMsg
-from .models import Bug, Message
+from .models import User, File, PhoneVerify
 
-# Register your models here.
-
-
-def register(models):
-    for i in models:
-        admin.site.register(i)
 
 class UserAdmin(admin.ModelAdmin):
- 
-    # 定义列表中要显示哪些字段
-    list_display = ['id', "username"]
-    search_fields = ['username']
+    list_display = ['id', 'username', 'isSuperUser', 'Verified', 'phone_number']
+    search_fields = ['username', 'phone_number']
+    list_filter = ['isSuperUser', 'Verified']
 
-admin.site.register(User, UserAdmin)
 
-register([
-    Tag, File, Plan,
-    Article, Comment, Paper, ChatMsg,
-    Bug, Message
-])
+class FileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'UserFrom', 'isPrivate', 'filePath']
+    search_fields = ['name', 'filePath', 'hash_code']
+    list_filter = ['isPrivate']
+
+
+class PhoneVerifyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'phone_number', 'hashCode', 'verify_code', 'created_at']
+    search_fields = ['phone_number', 'verify_code']
+    list_filter = ['created_at']
+
+
+def register(site=admin.site):
+    site.register(User, UserAdmin)
+    site.register(File, FileAdmin)
+    site.register(PhoneVerify, PhoneVerifyAdmin)
+
+
+register()
